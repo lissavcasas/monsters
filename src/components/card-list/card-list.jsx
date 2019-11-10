5b3ecb10fa6a25e6f1c0559b3f9ebf95a6f31e73
marimeli <marimel.casas@gmail.com>
2019-11-10 15:40:56 -0500

    card component

--- src/components/card-list/card-list.jsx
+++ src/components/card-list/card-list.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import Card from '../card/card'
 import './card-list.styles.css';
 
-const CardList = props => {
-  console.log(props)
-  return (
-    <div className='card-list'>{props.children}</div>
-  )
-}
+const CardList = props => (
+ /*  console.log(props) */
+    <div className='card-list'>
+      {/* ya no es this.state, sino props */}
+       {props.monsters.map(monster => (
+        <Card key={monster.id} monster={monster}/>
+          ))}
+    </div>
+  );
 
 export default CardList;
